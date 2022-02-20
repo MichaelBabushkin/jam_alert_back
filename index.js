84f32ea55e674b75ac8366ef730a253016521d2a
michael <mishaba1990@gmail.com>
2022-02-20 21:26:13 +0200

    back to index

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
-// const dbConfig = require("./config/db.config");
-// const db = require("./models");
-// var dotenv = require('dotenv');
-// dotenv.config();
+const cors = require("cors");
+const dbConfig = require("./config/db.config");
+const db = require("./models");
+var dotenv = require('dotenv');
+dotenv.config();
 
 
 // var corsOptions = {
-  //   origin: "http://localhost:3000"
-  // };
+//     origin: "http://localhost:3000"
+//   };
   
-  // var whitelist = ['http://localhost:3000', 'https://jam-alert.herokuapp.comג']
-  // var corsOptions = {
-  //   origin: function (origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       callback(null, true)
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'))
-  //     }
-  //   }
-  // }
+  var whitelist = ['http://localhost:3000', 'https://jam-alert.herokuapp.comג']
+  var corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
   
   // app.use(cors(corsOptions));
   
