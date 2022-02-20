9b0a95fd1698867b3f501d123502ac9c602da631
michael <mishaba1990@gmail.com>
2022-02-20 21:23:46 +0200

    back to index

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ const app = express();
   });
   
   // routes
-  require("./routes/auth.routes")(app);
-  require("./routes/user.routes")(app);
+  // require("./routes/auth.routes")(app);
+  // require("./routes/user.routes")(app);
   
   // set port, listen for requests
   const port = process.env.PORT || 8080;
