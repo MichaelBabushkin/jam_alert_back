a0003c7bd878085c5bb0cce971c649c1e0f5211b
michael <mishaba1990@gmail.com>
2022-02-20 22:32:21 +0200

    test log

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const dbConfig = require("./config/db.config");
-// const db = require("./models");
+const dbConfig = require("./config/db.config");
+const db = require("./models");
 var dotenv = require('dotenv');
 dotenv.config();
 
