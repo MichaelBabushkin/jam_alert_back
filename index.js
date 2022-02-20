91508af8ccc102bde8711e7c8afdaeddf1187da6
michael <mishaba1990@gmail.com>
2022-02-20 21:49:49 +0200

    test log

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ dotenv.config();
   app.use(cors(corsOptions));
   
   // parse requests of content-type - application/json
-  app.use(express.json());
+  // app.use(express.json());
   
   // parse requests of content-type - application/x-www-form-urlencoded
-  app.use(express.urlencoded({ extended: true }));
+  // app.use(express.urlencoded({ extended: true }));
   
   console.log("test log");
   
